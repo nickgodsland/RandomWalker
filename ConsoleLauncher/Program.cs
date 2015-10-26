b18c6918793bf3469612fb6051e0488e9666e5f2
Nicholas Godsland <Nicholas.Godsland@trayport.com>
2015-10-26 13:58:45 +0000

    Initial commit: Sln, projects, empty test

---
+++ ConsoleLauncher/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using RandomWalker2;
+
+namespace ConsoleLauncher
+{
+   class Program
+   {
+      private static void Main(string[] args)
+      {
+         var tests = new Tests();
+         tests.Test();
+         tests.TearDown();
+         Console.ReadKey();
+      }
+   }
+}
---
+++ RandomWalker2/Tests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Windows.Automation;
+using NUnit.Framework;
+
+namespace RandomWalker2
+{
+   [TestFixture]
+   public class Tests
+   {
+      [TearDown]
+      public void TearDown()
+      {
+
+      }
+
+      [Test]
+      public void Test()
+      {
+
+      }
+   }
+}

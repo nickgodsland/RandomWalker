735a209546e9367943f7dac6f330e531b3f2461b
Nicholas Godsland <Nicholas.Godsland@trayport.com>
2015-10-27 13:34:10 +0000

    Done

--- RandomWalker2/Tests.cs
+++ RandomWalker2/Tests.cs
@@ -11,6 +11,20 @@ namespace RandomWalker2
    [TestFixture]
    public class Tests
    {
+      private Process _process;
+      private AutomationElementCollection _automationElements;
+
+      private readonly string[] _elementBlackList =
+{
+         "Minimize", "Maximize", "Close", "System", "Calculator", "Application", "System Menu Bar"
+      };
+
+      private enum MouseEvent
+      {
+         MouseEventLeftDown = 0x02,
+         MouseEventLeftUp = 0x04
+      }
+
       [TearDown]
       public void TearDown()
       {
@@ -20,7 +34,88 @@ namespace RandomWalker2
       [Test]
       public void Test()
       {
+         StartProces();
 
+         var mainWindowAutomationElement = AutomationElement.FromHandle(_process.MainWindowHandle);
+
+         _automationElements = mainWindowAutomationElement.FindAll(TreeScope.Subtree, Condition.TrueCondition);
+
+         StructureChangedEventHandler structureChangedEventHandler = OnStructureChange;
+         Automation.AddStructureChangedEventHandler(mainWindowAutomationElement, TreeScope.Subtree,
+            structureChangedEventHandler);
+
+         var random = new Random();
+
+         while (!_process.HasExited && ProcessIsActiveWindow())
+         {
+            var randomAutomationElement = _automationElements[random.Next(_automationElements.Count - 1)];
+
+            try
+            {
+               Console.WriteLine($"Name: {randomAutomationElement.Current.Name} ClassName: {randomAutomationElement.Current.ClassName}");
+
+               if (CheckElementIsInvalid(randomAutomationElement))
+                  continue;
+
+               var location = randomAutomationElement.Current.BoundingRectangle.Location;
+
+               SetCursorPos((int)location.X, (int)location.Y);
+
+               Click((int)MouseEvent.MouseEventLeftDown, 0, 0, 0, 0);
+               Click((int)MouseEvent.MouseEventLeftUp, 0, 0, 0, 0);
+            }
+            catch (ElementNotAvailableException)
+            {
+               Console.WriteLine("ElementNotAvailableException");
+            }
+         }
       }
+
+      private void OnStructureChange(object sender, StructureChangedEventArgs e)
+      {
+         Console.WriteLine($"OnStructureChange {e.StructureChangeType}, {e.EventId}, {e.GetRuntimeId()}");
+
+         var automationElement = AutomationElement.FromHandle(_process.MainWindowHandle);
+         _automationElements = automationElement.FindAll(TreeScope.Subtree, Condition.TrueCondition);
+      }
+
+      private bool CheckElementIsInvalid(AutomationElement randomAutomationElement)
+      {
+         return randomAutomationElement.Current.BoundingRectangle.IsEmpty ||
+                randomAutomationElement.Current.IsOffscreen ||
+                _elementBlackList.Contains(randomAutomationElement.Current.Name);
+      }
+
+      private void StartProces(string name = "calc.exe")
+      {
+         _process = Process.Start(name);
+         while (_process.MainWindowHandle == IntPtr.Zero)
+         {
+         }
+      }
+
+      private bool ProcessIsActiveWindow()
+      {
+         var foregroundWindowHandle = GetForegroundWindow();
+         var isActive = _process.MainWindowHandle == foregroundWindowHandle || foregroundWindowHandle == IntPtr.Zero;
+
+         if (!isActive)
+         {
+            Console.WriteLine($"Process no longer has active window");
+         }
+
+         return isActive;
+      }
+
+      [DllImport("user32.dll")]
+      private static extern bool SetCursorPos(int positionX, int positionY);
+
+
+      [DllImport("user32.dll")]
+      private static extern IntPtr GetForegroundWindow();
+
+      [DllImport("User32.dll", EntryPoint = "mouse_event")]
+      private static extern void Click(int dwFlags, int dx, int dy, int cButtons, int dwExtraInfo);
+
    }
 }
